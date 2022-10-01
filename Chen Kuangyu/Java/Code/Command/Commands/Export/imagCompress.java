fcbfe9af0b9db693dedc61889858139fb35d160c
kynewbee <113176493+kynewbee@users.noreply.github.com>
2022-10-01 22:24:15 +0800

    Add files via upload

--- Chen Kuangyu/Java/Code/Command/Commands/Export/imagCompress.java
+++ Chen Kuangyu/Java/Code/Command/Commands/Export/imagCompress.java
@@ -1,17 +1,11 @@
 package Code.Command.Commands.Export;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
 
 import Code.Command.Base.*;
 import Code.Software.imgProcessor;
 
-import javax.imageio.ImageIO;
-import javax.imageio.stream.ImageOutputStream;
-
-import java.awt.image.BufferedImage;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -24,38 +18,27 @@ public class imagCompress extends Command{
         this.path=path;
     }
 
-    public InputStream getImageStream(BufferedImage bimage){
-        InputStream is = null;
-        ByteArrayOutputStream bs = new ByteArrayOutputStream();
-        ImageOutputStream imOut;
-        try {
-        imOut = ImageIO.createImageOutputStream(bs);
-        ImageIO.write(bimage, "png",imOut);
-        is= new ByteArrayInputStream(bs.toByteArray());
-        } catch (IOException e) {
-        e.printStackTrace();
-        }
-        return is;
-        }
+  
  
     public void compress() throws IOException {
+        FileInputStream file = new FileInputStream("C:\\Users\\chenkuangyu\\Desktop\\屏幕截图 2022-10-01 132127 - 副本.jpg");
         ZipOutputStream out = new ZipOutputStream(new FileOutputStream(path));//创建ZipoutputStream类对象
-        zip(out,getImageStream(iProcessor.getImg()),"");
-        out.close();//close
+        
+        out.putNextEntry(new ZipEntry("test.jpg"));
+
+        int len;
+        byte[] buffer = new byte[1024]; //字节数组大小可调节
+        //读取fis字节流，转移到buffer字节数组中去，读取后fis为空
+        while ((len = file.read(buffer)) > 0) {
+            out.write(buffer, 0, len);
+        } 
+		//关闭压缩包打包
+        out.closeEntry();
+        file.close();
+        out.close();
+
 
         
-    }
-
-    private void zip(ZipOutputStream out,InputStream in,String base) throws IOException {//方法重载
-       
-
-        out.putNextEntry(new ZipEntry(base));//创建新的进入点
-        int b;
-        System.out.println(base);
-        while ((b=in.read())!=-1){//如果没有达到流的尾部
-            out.write(b);//将字节写入当前ZIP条目
-        }
-        in.close();//close
     }
 
 
