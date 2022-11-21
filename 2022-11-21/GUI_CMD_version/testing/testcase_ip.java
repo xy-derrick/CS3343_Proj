9ce3eec702284044a40fe55987945b979071c166
kynewbee <113176493+kynewbee@users.noreply.github.com>
2022-11-21 23:55:20 +0800

    Add files via upload

---
+++ 2022-11-21/GUI_CMD_version/testing/testcase_ip.java
@@ -0,0 +1,34 @@
+package testing;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import Java.Code.Software.Software;
+import Java.Code.Software.imgProcessor;
+
+
+public class testcase_ip {
+	@Test
+	// Sample Test Case
+	public void test_ImgProcessor_01(){
+		imgProcessor ip = new imgProcessor("01");
+		ip.setImg(null);
+		assertEquals(ip.getName(),"01");
+	}
+	@Test
+	public void test_ImgProcessor_02(){
+		imgProcessor ip = new imgProcessor(1);
+		ip.setImg(null);
+		assertEquals(ip.getImg(),null);
+		
+	}
+	@Test
+	public void test_ImgProcessor_03(){
+		imgProcessor ip = new imgProcessor();
+		ip.setPath("4010");
+		assertEquals(ip.getPath(),"4010");
+	}
+	
+	
+	
+}
